4dcab418a26df7ff7c1c5465712fc645f6b9595d
주현준 <jhjk1234@gmail.com>
2025-01-28 09:42:59 +0900

    [Bronze V] Title: 시험 성적, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,13 @@
+import sys
+score = int(sys.stdin.readline())
+
+if 90 <= score <= 100:
+	print("A")
+elif score >= 80:
+	print("B")
+elif score >= 70:
+	print("C")
+elif score >= 60:
+	print("D")
+else:
+	print("F")
