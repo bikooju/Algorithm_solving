ea5f9a6a4846ee4ae0fe3c8fd254f5607326e5f2
주현준 <jhjk1234@gmail.com>
2025-02-04 17:58:02 +0900

    (풀이 추가)두 정수 사이의 합.py

--- 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
+++ 프로그래머스/1/12912. 두 정수 사이의 합/두 정수 사이의 합.py
@@ -10,3 +10,10 @@ def solution(a, b):
             answer += i
     
     return answer
+
+# programmers, phase1 : 두 정수 사이의 합, python
+def solution(a, b):
+    if a < b :
+        return sum(list(range(a, b+1)))
+    else:
+        return sum(list(range(b, a+1)))
