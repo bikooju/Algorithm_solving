0f8cb0c2f83cc6e6a9e7ff42fc75ff0b27138c61
주현준 <jhjk1234@gmail.com>
2025-03-06 12:54:16 +0900

    [Silver III] Title: 팰린드롬 만들기, Time: 64 ms, Memory: 34908 KB -BaekjoonHub

--- 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
+++ 백준/Silver/1213. 팰린드롬 만들기/팰린드롬 만들기.py
@@ -1,19 +1,21 @@
-import collections
 import sys
+from collections import Counter
 
-word = sys.stdin.readline().rstrip()
-check_word = collections.Counter(word)
+name = sys.stdin.readline().rstrip()
+checked_words = Counter(name)
 cnt = 0
-result = ''
 mid = ''
-for k, v in list(check_word.items()):
-    if v % 2 == 1: #홀수라면
+result = '' #앞부분
+
+for k,v in checked_words.items():
+    if v % 2 == 1:
         cnt += 1
-        mid = k #중간에 들어갈 값으로 저장
-        if cnt >= 2: #홀수가 2개이상이면 팰린드롬이 될 수 없다!!
+        mid = k
+        if cnt >= 2:
             print("I'm Sorry Hansoo")
             exit()
 
-for k, v in sorted(check_word.items()): #정렬을 통해 사전순으로 for문을 돌게함
-    result += (k * (v // 2)) #정확히 절반으로 나뉜 문자열을 만들어야 하므로 현재 갯수를 2로 나눠줌
-print(result + mid + result[::-1]) # 앞+중간+뒤 를 더해 문자열 출력
+for k,v in sorted(checked_words.items()):
+    result += k*(v // 2)
+
+print(result + mid + result[::-1])
