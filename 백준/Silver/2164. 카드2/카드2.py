5de90e47087c60ed7b0d81afbe141c2353b6e266
주현준 <jhjk1234@gmail.com>
2026-02-08 21:03:52 +0900

    [Silver IV] Title: 카드2, Time: 156 ms, Memory: 51848 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,9 @@
+import sys
+from collections import deque
+N = int(sys.stdin.readline())
+N_list = deque(range(1, N+1))
+while(len(N_list) > 1):
+    N_list.popleft()
+    N_list.append(N_list.popleft())
+
+print(N_list[0])
