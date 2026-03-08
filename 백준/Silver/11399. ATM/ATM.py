e8559d34f5260274ad078b2fd147e6c603f195be
주현준 <jhjk1234@gmail.com>
2026-03-08 19:55:04 +0900

    [Silver IV] Title: ATM, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/11399. ATM/ATM.py
+++ 백준/Silver/11399. ATM/ATM.py
@@ -1,15 +1,11 @@
 import sys
 N = int(sys.stdin.readline())
 sum_pi = 0
-result = 0
 pi_list = list(map(int, sys.stdin.readline().split())) # [3,1,4,3,2]
 pi_list.sort() # [1,2,3,3,4]
 sum_list = []
 for i in pi_list:
     sum_pi += i
     sum_list.append(sum_pi) # 0+1, 1+2,  1+2+3, 1+2+3+3, 1+2+3+3+4
-
-for i in sum_list: #[1, 3, 6, 9, 13]
-    result += i
-
+    
 print(sum(sum_list))
