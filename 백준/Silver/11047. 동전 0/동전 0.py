9f46afef91ecfa37af5b25e7158179bdc5f6538c
주현준 <jhjk1234@gmail.com>
2026-03-08 18:47:48 +0900

    [Silver IV] Title: 동전 0, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Silver/11047. 동전 0/동전 0.py
@@ -0,0 +1,31 @@
+import sys
+N, K = map(int, sys.stdin.readline().split())
+N_list = [int(sys.stdin.readline().rstrip()) for _ in range(N)]
+count = 0 # 동전 개수
+is_max = True
+max_n_count = 0
+lower_K = []
+
+
+
+#max_n 구하기
+for n in N_list:
+    if n > K:
+        break
+    lower_K.append(n) # [1,5,10,50,100]
+
+
+while K > 0:
+    # K : 200, max(lower_K): 100
+    if K >= max(lower_K):
+        # max_n을 몇개 곱할 수 있을지 (4200 / 1000 > 1)
+        max_n_count = K // max(lower_K)  # 200 // 100 => 2
+        K = K - (max(lower_K) * max_n_count)  # 200 - 100 * 2 = 0
+        count = count + max_n_count # count = 4 + 2 = 6
+
+    lower_K.pop() # 현재 최댓값 제거
+
+    if K == 0:
+        break
+
+print(count)
